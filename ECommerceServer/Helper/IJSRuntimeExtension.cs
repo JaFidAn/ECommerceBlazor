50fbf929e58731d6f7ca725b20e305d20362cf73
Rasim Alagezov <r.alagezov@gmail.com>
2022-10-08 11:52:10 +0400

    Lesson 6 - JavaScript

---
+++ ECommerceServer/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace ECommerceServer.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
